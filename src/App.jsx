9222df4502747ece4f15a445a10dde4cc2a7be74
D20CM <scottbrown500@hotmail.com>
2024-07-15 18:24:46 +0100

    add basic styling to placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -17,40 +17,45 @@ function App() {
           Instagram.
         </a>
       </p>
-      <img src={scottmainphoto} className="homepagephoto"></img>
-      <p>
-        Scott Brown is a Welsh musician who is currently based in Liverpool. He
-        is influenced as much by his background and early musical experiences
-        playing trombone in his local brass band as his favorite musicians.
-        These include folk musicians such as Bert Jansch and John Renbourn, jazz
-        musicians such as Pat Metheny and John Scofield and a host of others
-        including Bob Dylan, Tom Waits, Paul Simon, James Taylor, Stevie Ray
-        Vaughan, Michael Hedges, Tommy Emmanuel, Joe Pass, Django Reinhardt,
-        Muddy Waters, Son House, Guy Clarke, Martin Simpson, John Martyn, and
-        many more.{" "}
-      </p>
-      <p>
-        After a few years in London studying music at Goldsmiths University, he
-        spent most of his early career as a bassist and guitarist in the luxury
-        hotel and cruise scene, an experience which allowed him to travel to an
-        array of exotic locations whilst honing his craft. He is recently
-        relocated to Liverpool and is enjoying becoming familiar with the city’s
-        thriving acoustic music scene.
-      </p>
-      <p>
-        As a bassist, guitarist, vocalist, pianist, and trombonist, Scott’s
-        versatility and enthusiasm has made him a key ingredient to a host of
-        bands, shows, and productions worldwide. He is currently working as one
-        half of{" "}
-        <a
-          href="https://doublemaltduo.com/"
-          alt="Double Malt Duo website"
-          target="_blank"
-        >
-          Double Malt Duo
-        </a>{" "}
-        in a range of settings in the global entertainment industry.
-      </p>
+      <div className="main-area">
+        <img src={scottmainphoto} className="homepagephoto"></img>
+        <div className="about">
+          <h2>About</h2>
+          <p>
+            Scott Brown is a Welsh musician who is currently based in Liverpool.
+            He is influenced as much by his background and early musical
+            experiences playing trombone in his local brass band as his favorite
+            musicians. These include folk musicians such as Bert Jansch and John
+            Renbourn, jazz musicians such as Pat Metheny and John Scofield and a
+            host of others including Bob Dylan, Tom Waits, Paul Simon, James
+            Taylor, Stevie Ray Vaughan, Michael Hedges, Tommy Emmanuel, Joe
+            Pass, Django Reinhardt, Muddy Waters, Son House, Guy Clarke, Martin
+            Simpson, John Martyn, and many more.{" "}
+          </p>
+          <p>
+            After a few years in London studying music at Goldsmiths University,
+            he spent most of his early career as a bassist and guitarist in the
+            luxury hotel and cruise scene, an experience which allowed him to
+            travel to an array of exotic locations whilst honing his craft. He
+            is recently relocated to Liverpool and is enjoying becoming familiar
+            with the city’s thriving acoustic music scene.
+          </p>
+          <p>
+            As a bassist, guitarist, vocalist, pianist, and trombonist, Scott’s
+            versatility and enthusiasm has made him a key ingredient to a host
+            of bands, shows, and productions worldwide. He is currently working
+            as one half of{" "}
+            <a
+              href="https://doublemaltduo.com/"
+              alt="Double Malt Duo website"
+              target="_blank"
+            >
+              Double Malt Duo
+            </a>{" "}
+            in a range of settings in the global entertainment industry.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/Transcriptions.jsx
@@ -0,0 +1,27 @@
+function Transcriptions() {
+  return (
+    <div>
+      <h2>Transcriptions</h2>
+      <h3>Australian Tune</h3>
+      <p>
+        I’ve added a transcription of another of my compositions – a short tune
+        in 6/8 time which floats between G major and D major tonalities. It’s
+        called Australian Tune as it was written while I was backpacking in
+        Australia several years ago. It’s in an open Gmaj7 tuning – low to high
+        it’s DGDF#BD. You can find a video of me playing it here. I hope you
+        enjoy the melody and have fun exploring some of the chord shapes, let me
+        know how you get on. You can find more transcriptions below.
+      </p>
+      <h3>No Time Like The Past</h3>
+      <p>
+        Below is a transcription I made of one of my solo acoustic guitar
+        compositions – it’s a pretty little tune with some tapped harmonics and
+        palm muted bass string work to add interest to the texture. You can
+        watch an old video of me playing this here. I’m hoping to add a few more
+        of these transcriptions as soon as time allows. Hope you enjoy this one!
+      </p>
+    </div>
+  );
+}
+
+export default Transcriptions;
