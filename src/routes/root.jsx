9c167b7fea15f1c208b14e4febb4fa051d8ef576
D20CM <scottbrown500@hotmail.com>
2024-08-01 21:32:35 +0100

    add useOutlet hook in root.jsx to render /home where no route is specified

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,18 +1,21 @@
 import Sidebar from "../components/Sidebar";
 import MobileNav from "../components/MobileNav";
 import "../index.css";
-import { Outlet } from "react-router-dom";
+import { Outlet, useOutlet } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
+import Homepage from "../components/Homepage";
 
 function Root() {
   const isDesktopOrLaptop = useMediaQuery({
     query: "(min-width: 1100px)",
   });
 
+  const outlet = useOutlet();
+
   return (
     <section className="maincontainer">
       {isDesktopOrLaptop ? <Sidebar /> : <MobileNav />}
-      <Outlet />
+      {outlet || <Homepage />}
     </section>
   );
 }
