74084f2c82bd02bbd98dd4f04613d6205a5afb1e
D20CM <scottbrown500@hotmail.com>
2024-11-05 16:07:39 +0000

    add further photos to gallery

--- src/assets/img_library.js
+++ src/assets/img_library.js
@@ -11,6 +11,15 @@ import cove_piano from "../assets/images/instaphotos/80040820_457369565139434_18
 import xmas_palmcourt from "../assets/images/instaphotos/78934813_728108787676352_6447134012952752643_n_17876533150499170.jpg";
 import guitarist_sketch from "../assets/images/instaphotos/81655861_166124014680063_6530365461830625421_n_18087284170146577.jpg";
 import double_malt_seabourne from "../assets/images/instaphotos/59793148_591125304715866_2893164460669607793_n_17886728881337946.jpg";
+import osaka_window from "../assets/images/instaphotos/20065771_1287407231385937_7504789496685133824_n_17883597445066908.jpg";
+import crafter from "../assets/images/instaphotos/11313734_1413724558956757_270372100_n_17841912895030178.jpg";
+import palmcourt from "../assets/images/instaphotos/73385842_168833591024500_2977857903058773616_n_17923334800357281.jpg";
+import twin_guitars from "../assets/images/instaphotos/119048673_679547312769722_5490769878487184445_n_18076081687222838.jpg";
+import at_home from "../assets/images/instaphotos/69011804_164914544560533_3066576434660442583_n_18016244317227929.jpg";
+import mallorca from "../assets/images/instaphotos/vlcsnap-2024-11-05-04h13m26s948.png";
+import st_malo from "../assets/images/instaphotos/52188254_1351027018368795_8049297340786634217_n_18013360321121071.jpg";
+import cologne from "../assets/images/instaphotos/434990834_1596393164493408_7113099633520998426_n_18015554552329318.webp";
+import piano_flowers from "../assets/images/instaphotos/126190728_304336624014457_6032953164176561974_n_18173977168059635.jpg";
 
 const photoCollection = [
   storyhouse,
@@ -22,6 +31,15 @@ const photoCollection = [
   xmas_palmcourt,
   guitarist_sketch,
   double_malt_seabourne,
+  osaka_window,
+  crafter,
+  palmcourt,
+  twin_guitars,
+  at_home,
+  mallorca,
+  st_malo,
+  cologne,
+  piano_flowers,
 ];
 
 export default photoCollection;
