e2e5b0e0de1a0a96a0821aff7a410a2902d4b4ab
D20CM <scottbrown500@hotmail.com>
2024-11-05 15:02:02 +0000

    change image paths and imports for deployment

---
+++ src/assets/img_library.js
@@ -0,0 +1,27 @@
+import storyhouse from "../assets/images/instaphotos/464162957_1066837571330798_2028153425789983948_n_18145155919344469.webp";
+
+import wedding_guitar from "../assets/images/instaphotos/457405275_18459672103030178_8774386731234122143_n_18034881035174375.webp";
+
+import trips_for_biscuits from "../assets/images/instaphotos/301764139_1979643438897167_7141968644634529731_n_17974574932725484.webp";
+
+import winter_garden from "../assets/images/instaphotos/352464924_575949274654689_234236461115183600_n_17980963748193399.webp";
+
+import red_tie from "../assets/images/instaphotos/22071208_119019432146750_673384574897618944_n_17894961046069416.jpg";
+import cove_piano from "../assets/images/instaphotos/80040820_457369565139434_1871156683632179495_n_17856082552670631.jpg";
+import xmas_palmcourt from "../assets/images/instaphotos/78934813_728108787676352_6447134012952752643_n_17876533150499170.jpg";
+import guitarist_sketch from "../assets/images/instaphotos/81655861_166124014680063_6530365461830625421_n_18087284170146577.jpg";
+import double_malt_seabourne from "../assets/images/instaphotos/59793148_591125304715866_2893164460669607793_n_17886728881337946.jpg";
+
+const photoCollection = [
+  storyhouse,
+  wedding_guitar,
+  trips_for_biscuits,
+  winter_garden,
+  red_tie,
+  cove_piano,
+  xmas_palmcourt,
+  guitarist_sketch,
+  double_malt_seabourne,
+];
+
+export default photoCollection;
